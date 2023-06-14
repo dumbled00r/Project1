4d0a454e18db4f67e66a563df832acae28be1573
dumbled00r <kazuu.promotion@gmail.com>
2023-06-14 15:22:53 +0700

    Fix: getting Basic Group's members

---
+++ services/GetChat.java
@@ -0,0 +1,9 @@
+package services;
+/**
+Get Group Chat's Information
+ */
+public class GetGroupChat extends Base {
+    public static void getGroupChat(String[] args){
+
+    }
+}
--- services/GetMember.java
+++ services/GetMember.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetMember extends Base {
+    /**
+    Get the members' userid of a group chat
+     */
     public static void getMember(String[] args)
     {
         client.send(new TdApi.GetChat(ConvertToLong.toLong(args[0])), new Client.ResultHandler() {
@@ -16,10 +19,11 @@ public class GetMember extends Base {
             public void onResult(TdApi.Object object) {
                 if (object instanceof TdApi.Chat) {
                     TdApi.Chat chat = (TdApi.Chat) object;
-                    if (((TdApi.ChatTypeSupergroup) chat.type).isChannel) {
-                        System.out.println("This chat group is a channel, cannot get members");
-                    }
+
                     if (chat.type instanceof TdApi.ChatTypeSupergroup) {
+                        if (((TdApi.ChatTypeSupergroup) chat.type).isChannel) {
+                            System.out.println("This chat group is a channel, cannot get members");
+                        }
                         long supergroupId = ((TdApi.ChatTypeSupergroup) chat.type).supergroupId;
                         client.send(new TdApi.GetSupergroupMembers(supergroupId, null, 0, 200), new Client.ResultHandler() {
                             @Override
@@ -32,26 +36,26 @@ public class GetMember extends Base {
                                             chatMemberIds.add(((TdApi.MessageSenderUser) member.memberId).userId);
                                         }
                                     }
-                                    System.out.println(chatMemberIds);
+                                    GetUser.getMassUser(chatMemberIds);
                                 }
                             }
                         });
                     } else if (chat.type instanceof TdApi.ChatTypeBasicGroup){
                         // Upgrade to Super group to get member
-                        client.send(new TdApi.UpgradeBasicGroupChatToSupergroupChat(chat.id), defaultHandler);
-                        long supergroupId = ((TdApi.ChatTypeSupergroup) chat.type).supergroupId;
-                        client.send(new TdApi.GetSupergroupMembers(supergroupId, null, 0, 200), new Client.ResultHandler() {
+                        TdApi.ChatTypeBasicGroup basicGroup = (TdApi.ChatTypeBasicGroup) chat.type;
+                        client.send(new TdApi.GetBasicGroupFullInfo(basicGroup.basicGroupId), new Client.ResultHandler() {
                             @Override
                             public void onResult(TdApi.Object object) {
-                                if (object instanceof TdApi.ChatMembers) {
-                                    TdApi.ChatMember[] chatMembers = ((TdApi.ChatMembers) object).members;
+                                if (object instanceof TdApi.BasicGroupFullInfo){
+
+                                    TdApi.ChatMember[] chatMembers = ((TdApi.BasicGroupFullInfo) object ).members;
                                     List<Long> chatMemberIds = new ArrayList<>();
                                     for (TdApi.ChatMember member : chatMembers) {
                                         if (member.memberId instanceof TdApi.MessageSenderUser) {
                                             chatMemberIds.add(((TdApi.MessageSenderUser) member.memberId).userId);
                                         }
                                     }
-                                    System.out.println(chatMemberIds);
+                                    GetUser.getMassUser(chatMemberIds);
                                 }
                             }
                         });
