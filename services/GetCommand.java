a2de5658b55ea78f9f17ba76e26a44ebff59538e
dumbled00r <kazuu.promotion@gmail.com>
2023-06-14 11:52:54 +0700

    Update commands

--- services/GetCommand.java
+++ services/GetCommand.java
@@ -6,7 +6,7 @@ import org.drinkless.tdlib.TdApi;
 public class GetCommand extends Base {
     protected static void getCommand() {
         String command = PromptString.promptString(commandsLine);
-        String[] commands = command.split(" ", 2);
+        String[] commands = command.split(" ", 3);
         try {
             switch (commands[0]) {
                 case "help":{
@@ -18,6 +18,7 @@ public class GetCommand extends Base {
                     System.out.println("add <ChatId> <UserId> - Add User To An Existing Chat");
                     System.out.println("link <ChatId> <AdminId> - Get Invite Link To An Existing Chat");
                     System.out.println("pm <UserId> - Send Private Message To User ");
+                    System.out.println("getmem <ChatId> - Get Members Of A Chat Group");
                     System.out.println("lo - Logout");
                     System.out.println("q - Quit");
                     break;
@@ -39,7 +40,7 @@ public class GetCommand extends Base {
                     break;
                 }
                 case "sm": {
-                    String[] args = commands[1].split(" ", 2);
+                    String[] args = commands[1].split(" ", 3);
                     SendMessage.sendMessage(ConvertToLong.toLong(args[0]), args[1]);
                     break;
                 }
