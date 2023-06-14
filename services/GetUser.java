e5fa25acfba53c01f8c55682c205ee03d79568da
dumbled00r <kazuu.promotion@gmail.com>
2023-06-14 15:30:08 +0700

    Added mass users' info getting

--- services/GetUser.java
+++ services/GetUser.java
@@ -3,25 +3,35 @@ package services;
 import org.drinkless.tdlib.Client;
 import org.drinkless.tdlib.TdApi;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GetUser extends Base {
-    public static void getUser(String[] args) {
-        TdApi.GetUser getUser = new TdApi.GetUser(ConvertToLong.toLong(args[0]));
+    /**
+     *
+     * Get single user
+     */
+    public static void getUser(String args) {
+        TdApi.GetUser getUser = new TdApi.GetUser(ConvertToLong.toLong(args));
         client.send(getUser, object -> {
             if (object.getConstructor() == TdApi.User.CONSTRUCTOR) {
                 TdApi.User user = (TdApi.User) object;
                 long id = user.id;
                 String firstName = user.firstName;
                 String lastName = user.lastName;
-                String languageCode = user.languageCode;
                 String userName = user.usernames.activeUsernames[0];
-                System.out.println("User ID: " + id);
+                System.out.println("\nUser ID: " + id);
                 System.out.println("Username: " + userName);
                 System.out.println("First Name: " + firstName);
-                System.out.println("Last Name: " + lastName);
-                System.out.println("Language Code: " + languageCode);
+                System.out.println("Last Name: " + lastName + "\n");
             } else {
                 System.out.println("Failed to get user: " + object);
             }
         });
+    }
+    public static void getMassUser(List<Long> userIds){
+        for (Long userId : userIds){
+            getUser(Long.toString(userId));
+        }
     }
 }
