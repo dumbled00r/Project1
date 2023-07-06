c0206ffe1702fb3e129d6631c767e75e9f36f853
duyanh3101 <kazuu.promotion@gmail.com>
2023-07-07 00:50:10 +0700

    Update console

--- src/main/java/Services/GetUser.java
+++ src/main/java/Services/GetUser.java
@@ -8,6 +8,7 @@ import com.google.gson.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GetUser extends Base {
     private static List<JsonObject> lstJsonResults = new ArrayList<>();
@@ -36,7 +37,6 @@ public class GetUser extends Base {
                 jsonResults.addProperty("First Name", firstName);
                 jsonResults.addProperty("Last Name", lastName);
                 jsonResults.addProperty("Chat Id", chatId);
-                System.out.println(jsonResults);
             } else {
                 System.out.println("Failed to get user: " + object);
             }
@@ -49,12 +49,31 @@ public class GetUser extends Base {
      * @param userIds
      * @param chatId
      */
+    private static final ReentrantLock lock = new ReentrantLock();
+
     public static List<JsonObject> getMassUser(List<Long> userIds, Long chatId) throws InterruptedException {
-        for (Long userId : userIds){
+        for (Long userId : userIds) {
             JsonObject data = getUser(userId, chatId);
             if (!data.isJsonNull()) {
                 lstJsonResults.add(data);
             }
+        }
+        if (!lstJsonResults.isEmpty()) {
+            System.out.println("User information:");
+            System.out.println("+------+----------------+---------------+---------------+-----------------+");
+            System.out.println("|  ID  |    Username    |   First Name  |   Last Name   |     Chat ID     |");
+            System.out.println("+------+----------------+---------------+---------------+-----------------+");
+            for (JsonObject result : lstJsonResults) {
+                long id = result.get("Id").getAsLong();
+                String username = result.get("Username").getAsString();
+                String firstName = result.get("First Name").getAsString();
+                String lastName = result.get("Last Name").getAsString();
+                long chatIdValue = result.get("Chat Id").getAsLong();
+                System.out.printf("| %-4d | %-14s | %-13s | %-13s | %-15d |\n", id, username, firstName, lastName, chatIdValue);
+            }
+            System.out.println("+------+----------------+---------------+---------------+-----------------+");
+        } else {
+            System.out.println("No user information available");
         }
         return lstJsonResults;
     }
