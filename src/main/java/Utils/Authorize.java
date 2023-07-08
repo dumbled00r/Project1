749d7d1cea88a86cb789b04096ebe9f10d90e543
duyanh3101 <kazuu.promotion@gmail.com>
2023-07-08 21:56:24 +0700

    Update code

--- src/main/java/Utils/Authorize.java
+++ src/main/java/Utils/Authorize.java
@@ -3,6 +3,7 @@ package Utils;
 import org.drinkless.tdlib.Client;
 import org.drinkless.tdlib.TdApi;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -11,7 +12,7 @@ public class Authorize extends Base{
 
 
 
-    public static void onAuthorizationStateUpdated(TdApi.AuthorizationState authorizationState) {
+    public static void onAuthorizationStateUpdated(TdApi.AuthorizationState authorizationState) throws ExecutionException, InterruptedException {
         if (authorizationState != null) {
             Authorize.authorizationState = authorizationState;
         }
@@ -21,8 +22,8 @@ public class Authorize extends Base{
                 request.databaseDirectory = "tdlib";
                 request.useMessageDatabase = true;
                 request.useSecretChats = true;
-                request.apiId = 21253741;
-                request.apiHash = "9df061bb226225982dad3aa34ae47647";
+                request.apiId = 26486494;
+                request.apiHash = "14209c4ff6103f138c89313486a36661";
                 request.systemLanguageCode = "en";
                 request.deviceModel = "Desktop";
                 request.applicationVersion = "1.0";
@@ -31,7 +32,7 @@ public class Authorize extends Base{
                 client.send(request, new Handler.AuthorizationRequestHandler());
                 break;
             case TdApi.AuthorizationStateWaitPhoneNumber.CONSTRUCTOR: {
-                String phoneNumber = String.valueOf(PromptString.promptStringAsync("Please enter phone number: "));
+                String phoneNumber = String.valueOf(PromptString.promptStringAsync("Please enter phone number: ").get());
                 client.send(new TdApi.SetAuthenticationPhoneNumber(phoneNumber, null), new Handler.AuthorizationRequestHandler());
                 break;
             }
@@ -41,28 +42,28 @@ public class Authorize extends Base{
                 break;
             }
             case TdApi.AuthorizationStateWaitEmailAddress.CONSTRUCTOR: {
-                String emailAddress = String.valueOf(PromptString.promptStringAsync("Please enter email address: "));
+                String emailAddress = String.valueOf(PromptString.promptStringAsync("Please enter email address: ").get());
                 client.send(new TdApi.SetAuthenticationEmailAddress(emailAddress), new Handler.AuthorizationRequestHandler());
                 break;
             }
             case TdApi.AuthorizationStateWaitEmailCode.CONSTRUCTOR: {
-                String code = String.valueOf(PromptString.promptStringAsync("Please enter email authentication code: "));
+                String code = String.valueOf(PromptString.promptStringAsync("Please enter email authentication code: ").get());
                 client.send(new TdApi.CheckAuthenticationEmailCode(new TdApi.EmailAddressAuthenticationCode(code)), new Handler.AuthorizationRequestHandler());
                 break;
             }
             case TdApi.AuthorizationStateWaitCode.CONSTRUCTOR: {
-                String code = String.valueOf(PromptString.promptStringAsync("Please enter authentication code: "));
+                String code = String.valueOf(PromptString.promptStringAsync("Please enter authentication code: ").get());
                 client.send(new TdApi.CheckAuthenticationCode(code), new Handler.AuthorizationRequestHandler());
                 break;
             }
             case TdApi.AuthorizationStateWaitRegistration.CONSTRUCTOR: {
-                String firstName = String.valueOf(PromptString.promptStringAsync("Please enter your first name: "));
+                String firstName = String.valueOf(PromptString.promptStringAsync("Please enter your first name: ").get());
                 String lastName = String.valueOf(PromptString.promptStringAsync("Please enter your last name: "));
                 client.send(new TdApi.RegisterUser(firstName, lastName), new Handler.AuthorizationRequestHandler());
                 break;
             }
             case TdApi.AuthorizationStateWaitPassword.CONSTRUCTOR: {
-                String password = String.valueOf(PromptString.promptStringAsync("Please enter password: "));
+                String password = String.valueOf(PromptString.promptStringAsync("Please enter password: ").get());
                 client.send(new TdApi.CheckAuthenticationPassword(password), new Handler.AuthorizationRequestHandler());
                 break;
             }
--- src/main/java/Utils/Handler.java
+++ src/main/java/Utils/Handler.java
@@ -4,6 +4,8 @@ import com.google.gson.*;
 import org.drinkless.tdlib.Client;
 import org.drinkless.tdlib.TdApi;
 
+import java.util.concurrent.ExecutionException;
+
 public class Handler extends Base {
     public static class DefaultHandler implements Client.ResultHandler {
         @Override
@@ -15,7 +17,7 @@ public class Handler extends Base {
     }
     public static class UpdateHandler implements Client.ResultHandler {
         @Override
-        public void onResult(TdApi.Object object) {
+        public void onResult(TdApi.Object object) throws ExecutionException, InterruptedException {
             switch (object.getConstructor()) {
                 case TdApi.UpdateAuthorizationState.CONSTRUCTOR:
                     Authorize.onAuthorizationStateUpdated(((TdApi.UpdateAuthorizationState) object).authorizationState);
@@ -233,7 +235,7 @@ public class Handler extends Base {
 
     public static class AuthorizationRequestHandler implements Client.ResultHandler {
         @Override
-        public void onResult(TdApi.Object object) {
+        public void onResult(TdApi.Object object) throws ExecutionException, InterruptedException {
             switch (object.getConstructor()) {
                 case TdApi.Error.CONSTRUCTOR:
                     System.err.println("Receive an error:" + newLine + object);
