b722f79c53ceda15f8536974ee6c298d129da20f
duyanh3101 <kazuu.promotion@gmail.com>
2023-07-08 17:44:53 +0700

    Update get messages history

--- src/main/java/Services/GetMessagesHistory.java
+++ src/main/java/Services/GetMessagesHistory.java
@@ -4,52 +4,55 @@ import Utils.Base;
 import Utils.Print;
 import org.drinkless.tdlib.Client;
 import org.drinkless.tdlib.TdApi;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 public class GetMessagesHistory extends Base {
+
     public static CompletableFuture<List<TdApi.Message>> getMessages(Long chatId) {
         CompletableFuture<List<TdApi.Message>> future = new CompletableFuture<>();
 
-        TdApi.GetChatHistory getChatHistory = new TdApi.GetChatHistory(chatId, 0, 0, 100, false);
-        client.send(getChatHistory, new Client.ResultHandler() {
-            @Override
-            public void onResult(TdApi.Object object) {
-                if (object instanceof TdApi.Messages) {
-                    TdApi.Messages messages = (TdApi.Messages) object;
-                    List<TdApi.Message> messageList = Arrays.asList(messages.messages);
-                    future.complete(messageList);
-                } else {
-                    future.completeExceptionally(new RuntimeException("Failed to get messages"));
-                }
-            }
-        });
+        int limit = 100;
+        final int[] offset = {0};
+        final boolean[] reachedEnd = {false};
+        Set<Long> receivedMessageIds = new HashSet<>();
+        List<TdApi.Message> messagesList = new ArrayList<>();
 
-        return future.thenCompose(messages -> {
-            int offset = (int) messages.get(messages.size() - 1).id; // get the ID of the last message
-            TdApi.GetChatHistory getChatHistory2 = new TdApi.GetChatHistory(chatId, offset, 0, 100, false);
-            CompletableFuture<List<TdApi.Message>> future2 = new CompletableFuture<>();
-            client.send(getChatHistory2, new Client.ResultHandler() {
+        while(!reachedEnd[0]) {
+            TdApi.GetChatHistory getChatHistory = new TdApi.GetChatHistory(chatId, 0, offset[0], limit, false);
+            client.send(getChatHistory, new Client.ResultHandler() {
                 @Override
                 public void onResult(TdApi.Object object) {
                     if (object instanceof TdApi.Messages) {
                         TdApi.Messages messages = (TdApi.Messages) object;
-                        List<TdApi.Message> messageList = Arrays.asList(messages.messages);
-                        future2.complete(messageList);
+                        for (TdApi.Message message : messages.messages) {
+                            if (!receivedMessageIds.contains(message.id)) {
+                                messagesList.add(message);
+                                receivedMessageIds.add(message.id);
+                            }
+                        }
+                        if (messages.messages.length < limit) {
+                            reachedEnd[0] = true;
+                            future.complete(messagesList);
+                        } else {
+                            offset[0] += limit;
+                        }
                     } else {
-                        future2.completeExceptionally(new RuntimeException("Failed to get messages"));
+                        future.completeExceptionally(new RuntimeException("Failed to get messages"));
                     }
                 }
             });
-            return future2;
-        });
+        }
+
+        return future;
     }
+
     public static void printMessages(Long chatId) {
         getMessages(chatId).thenAccept(messages -> {
             System.out.println("Total messages: " + messages.size());
             for (TdApi.Message message : messages) {
-                System.out.println(message);
+                System.out.println(message.content);
             }
             Print.print("");
         });
